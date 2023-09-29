948a9c145cc4e88e10e9f23d2b6c751e6dc1f6c5
Adam <adambaldyga@gmail.com>
2023-09-29 13:55:56 +0200

    Add TODO list

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO 1: Kalkulator pyta o działanie.
+# TODO 2: Kalkulator pyta o dwie liczby.
+# TODO 3: Kalkulator informuje użytkownika, jakie działanie wykonamy i jakie będą jego argumenty (np. Dodaję 1 i 3).
+# TODO 4: Kalkulator wykonuje obliczenie i drukuje rezultat z print.
+# TODO 5: Sprawdzaj, czy podana wartość na pewno jest liczbą.
+# TODO 6: W wypadku mnożenia i dodawania daj użytkownikowi możliwość wpisania większej ilości argumentów niż tylko dwa,
+#         np. możesz dodać do siebie trzy i więcej liczb.
