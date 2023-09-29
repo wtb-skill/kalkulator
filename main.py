cf5549c2f7ba4f3a7fa28de6db8b45d50889389f
Adam <adambaldyga@gmail.com>
2023-09-29 15:21:01 +0200

    Add basic math functions (addition, subtraction, multiplication, division), dictionary named operations, which contains those functions and script to work as a basic calculator

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
+def addition(num_1: float, num_2: float) -> float:
+    return num_1 + num_2
 
 
+def subtraction(num_1: float, num_2: float) -> float:
+    return num_1 - num_2
 
 
+def multiplication(num_1: float, num_2: float) -> float:
+    return num_1 * num_2
 
 
+def division(num_1: float, num_2: float) -> float:
+    return num_1 / num_2
 
 
+operations = {
+    "1": (addition, 'Dodaję'),
+    "2": (subtraction, 'Odejmuję'),
+    "3": (multiplication, 'Mnożę'),
+    "4": (division, 'Dzielę')
+}
 
 
+if __name__ == "__main__":
+    operator = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, "
+                     "4 Dzielenie: ")
+    num_1 = float(input("Podaj pierwszą liczbę: "))
+    num_2 = float(input("Podaj drugą liczbę: "))
+    logging.info(f"{operations[operator][1]} {num_1} i {num_2}.")
+    outcome = operations[operator][0](num_1, num_2)
+    logging.info(f"Wynik to {outcome}.")
 
 
-# TODO 1: Kalkulator pyta o działanie.
-# TODO 2: Kalkulator pyta o dwie liczby.
-# TODO 3: Kalkulator informuje użytkownika, jakie działanie wykonamy i jakie będą jego argumenty (np. Dodaję 1 i 3).
-# TODO 4: Kalkulator wykonuje obliczenie i drukuje rezultat z print.
 # TODO 5: Sprawdzaj, czy podana wartość na pewno jest liczbą.
 # TODO 6: W wypadku mnożenia i dodawania daj użytkownikowi możliwość wpisania większej ilości argumentów niż tylko dwa,
 #         np. możesz dodać do siebie trzy i więcej liczb.
