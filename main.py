d125eb20db57b14a0dcefdbe30466d63fbd38fe4
Adam <adambaldyga@gmail.com>
2023-10-03 10:58:57 +0200

    Implement User Input Verification (TODO 5.)

--- main.py
+++ main.py
@@ -28,15 +28,33 @@ operations = {
 
 
 if __name__ == "__main__":
-    operator = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, "
-                     "4 Dzielenie: ")
-    num_1 = float(input("Podaj pierwszą liczbę: "))
-    num_2 = float(input("Podaj drugą liczbę: "))
-    logging.info(f"{operations[operator][1]} {num_1} i {num_2}.")
-    outcome = operations[operator][0](num_1, num_2)
+
+    while True:
+        operator = input(
+            "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+        if operator in ['1', '2', '3', '4']:
+            break  # Break out of the loop if a valid number is entered
+        else:
+            logging.info("To nie jest odpowiednia liczba! Podaj 1, 2, 3 lub 4.")
+
+    while True:
+        try:
+            number_1 = float(input("Podaj pierwszą liczbę: "))
+            break  # Break out of the loop if a valid number is entered
+        except ValueError:
+            logging.info("To nie jest liczba!")
+
+    while True:
+        try:
+            number_2 = float(input("Podaj drugą liczbę: "))
+            break  # Break out of the loop if a valid number is entered
+        except ValueError:
+            logging.info("To nie jest liczba!")
+
+    logging.info(f"{operations[operator][1]} {number_1} i {number_2}.")
+    outcome = operations[operator][0](number_1, number_2)
     logging.info(f"Wynik to {outcome}.")
 
 
-# TODO 5: Sprawdzaj, czy podana wartość na pewno jest liczbą.
 # TODO 6: W wypadku mnożenia i dodawania daj użytkownikowi możliwość wpisania większej ilości argumentów niż tylko dwa,
 #         np. możesz dodać do siebie trzy i więcej liczb.
